b96295d9a0c1b4d4ac1f99b02ea727b7ab105137
Gowtham K <gowthamk91@outlook.com>
2017-04-23 11:24:24 +0530

    Add project files.

---
+++ MyCoreAPI/Controllers/TechnologiesController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyCoreAPI.Model;
+using System.Net.Http;
+using System.Net;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MyCoreAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Technologies")]
+    public class TechnologiesController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("TechnologiesList")]
+        public List<TechnologyList> GetTechnology()
+        {
+            try
+            {
+                 List<TechnologyList> _TechList = new List<TechnologyList>();
+                _TechList.Add(new TechnologyList(1, "ASP.NET"));
+                _TechList.Add(new TechnologyList(2, "ADO.NET"));
+                _TechList.Add(new TechnologyList(3, "SilverLight"));
+                _TechList.Add(new TechnologyList(4, "C#"));
+                _TechList.Add(new TechnologyList(5, "SQL Server"));
+
+                //return Request.CreateResponse(HttpStatusCode.OK, _TechList, Configuration.Formatters.JsonFormatter);
+                return _TechList;
+
+            }
+            catch (Exception ex)
+            {
+                List < TechnologyList > _Tech= null;
+                return _Tech;
+            }
+        }
+    }
+}
---
+++ MyCoreAPI/Model/TechnologyList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCoreAPI.Model
+{
+    public class TechnologyList
+    {
+        public TechnologyList(int Value, string Text)
+        {
+
+            this.Value = Value;
+            this.Text = Text;
+        }
+        public int Value { get; set; }
+        public string Text { get; set; }
+    }
+}
