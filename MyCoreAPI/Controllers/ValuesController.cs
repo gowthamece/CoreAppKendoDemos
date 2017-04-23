0132eb50ae35f24e265bf4f60892af32df120186
Gowtham K <gowthamk91@outlook.com>
2017-04-23 21:21:21 +0530

    Change in List

--- MyCoreAPI/Controllers/ValuesController.cs
+++ MyCoreAPI/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace MyCoreAPI.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value1.1" };
+            return new string[] { "value1", "value1.2" };
         }
 
         // GET api/values/5
