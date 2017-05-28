80bdcc2bd0a5da83d3dbdcefe92d25d5500cdb51
Gowtham K <gowthamk91@outlook.com>
2017-05-28 10:06:39 +0530

    Enabled Cross Domain Call

--- MyCoreAPI/Startup.cs
+++ MyCoreAPI/Startup.cs
@@ -28,12 +28,22 @@ namespace MyCoreAPI
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
+            
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseCors("CorsPolicy");
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
