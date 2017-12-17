9e3960f202c13f0d3a684e539efe6e7434ab421b
Gowtham K <gowthamk91@outlook.com>
2017-12-17 12:07:31 +0530

    Added Kendo TreeView Example

---
+++ MyCoreAPI/Controllers/AutomobilesController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net;
+
+namespace MyCoreAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Automobiles")]
+    public class AutomobilesController : Controller
+    {
+        [HttpGet]
+        [Route("DataSource")]
+        public JsonResult HierachyDataSource()
+        {
+            var obj = new []
+            {
+                new {
+                name = "Automobiles",
+                value = 1,
+                items = new[] {
+                new {
+                    name = "Bus",
+                    value = 2,
+                    items = new dynamic [] {
+                        new {
+                            name = "Ford",
+                            value=3
+                        },
+                            new {
+                            name = "BMW",
+                            value=4,
+                        },
+
+                        },
+                    },
+
+                new {
+                    name = "Car",
+                    value=5,
+                    items = new dynamic [] {
+                        new { name = "Honda", value=6},
+                        new { name = "Ford",  value=7},
+
+                    }
+                },
+            },
+                },
+            };
+            return Json(obj);
+
+      
+        }
+    }
+}
