465ba582a895316b91b5ff4cec5afb4cc10cb730
Gowtham K <gowthamk91@outlook.com>
2018-01-28 12:07:02 +0530

    Added - autocomplete html file

--- MyCoreAPI/Startup.cs
+++ MyCoreAPI/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 
 namespace MyCoreAPI
@@ -46,6 +48,14 @@ namespace MyCoreAPI
             app.UseCors("CorsPolicy");
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+            Path.Combine(Directory.GetCurrentDirectory(), "JS")),
+                RequestPath = "/JS"
+            });
+          
 
             app.UseMvc();
         }
